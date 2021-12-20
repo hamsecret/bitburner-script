2333eeb1fb2d8b556c44c419ea854bf2f5393bbb
chenzhihan <chenzhihan93@126.com>
2021-12-20 14:51:29 +0800

    修改提示方式

--- czhAutoRun.js
+++ czhAutoRun.js
@@ -1,13 +1,13 @@
 async function basicHack(ns, hostname) {
     while (true) {
         if (ns.getServerSecurityLevel(hostname) > ns.getServerMinSecurityLevel(hostname)*2) {
-            ns.tprint('weak : '+hostname)
+            ns.toast('weak : '+hostname)
             await ns.weaken(hostname);
         } else if (ns.getServerMoneyAvailable(hostname) < ns.getServerMaxMoney(hostname)/3) {
-            ns.tprint('grow : '+hostname)
+            ns.toast('grow : '+hostname)
             await ns.grow(hostname);
         } else {
-            ns.tprint('hack : '+hostname)
+            ns.toast('hack : '+hostname)
             await ns.hack(hostname);
         }
     }
