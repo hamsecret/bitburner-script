63df9eafe6902cb10ea8c837d6331f295a4f15b1
chenzhihan <chenzhihan93@126.com>
2021-12-20 14:23:51 +0800

    修改了自动hack的阈值

--- czhAutoRun.js
+++ czhAutoRun.js
@@ -1,10 +1,13 @@
 async function basicHack(ns, hostname) {
     while (true) {
-        if (ns.getServerSecurityLevel(hostname) > ns.getServerMinSecurityLevel(hostname)) {
+        if (ns.getServerSecurityLevel(hostname) > ns.getServerMinSecurityLevel(hostname)*2) {
+            ns.tprint('weak : '+hostname)
             await ns.weaken(hostname);
-        } else if (ns.getServerMoneyAvailable(hostname) < ns.getServerMaxMoney(hostname)) {
+        } else if (ns.getServerMoneyAvailable(hostname) < ns.getServerMaxMoney(hostname)/3) {
+            ns.tprint('grow : '+hostname)
             await ns.grow(hostname);
         } else {
+            ns.tprint('hack : '+hostname)
             await ns.hack(hostname);
         }
     }
